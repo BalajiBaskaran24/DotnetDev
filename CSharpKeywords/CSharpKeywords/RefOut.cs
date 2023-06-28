dc25f892ceea2917f4f6b65b20a089582e8eef4c
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-06-28 22:08:30 +0530

    Create RefOut.cs

---
+++ CSharpKeywords/CSharpKeywords/RefOut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpKeywords
+{
+    /// <summary>
+    /// Ref and out keywords
+    /// </summary>
+    internal class RefOut
+    {
+    }
+}
