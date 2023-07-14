075d2862333e7131e9e294be15f7d5d1022441b4
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-07-14 08:35:49 +0530

    Stack implementation using list and array

---
+++ DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using DSAndAlgoReference;
+
+public enum Option
+{
+    Stack_List,
+    Stack_Array,
+    None
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Option ReqType = Option.None;
+        switch (ReqType)
+        {
+            case Option.Stack_List:
+                StackLst<int> stackLst = new();
+
+                stackLst.Push(1);
+                stackLst.Push(2);
+                stackLst.Push(3);
+
+                Console.WriteLine(stackLst.Peek());  // Output: 3
+
+                int poppedItem = stackLst.Pop();
+                Console.WriteLine(poppedItem);    // Output: 3
+
+                Console.WriteLine(stackLst.Peek());  // Output: 2
+                break;
+            case Option.Stack_Array:
+                StackArr<int> stackArr = new(5);
+
+                stackArr.Push(1);
+                stackArr.Push(2);
+                stackArr.Push(3);
+
+                Console.WriteLine(stackArr.Peek());  // Output: 3
+
+                poppedItem = stackArr.Pop();
+                Console.WriteLine(poppedItem);    // Output: 3
+
+                Console.WriteLine(stackArr.Peek());  // Output: 2
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/StackLst.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAndAlgoReference
+{
+    #region Using linked list implementation
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    /// <summary>
+    /// Stack implementation using linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class StackLst<T>
+    {
+        private Node<T> top;
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("StackLst is empty");
+            }
+
+            T data = top.Data;
+            top = top.Next;
+            return data;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("StackLst is empty");
+            }
+
+            return top.Data;
+        }
+    }
+
+    #endregion
+
+    #region Using Array implementation
+
+    /// <summary>
+    /// Stack implementation using array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class StackArr<T>
+    {
+        private T[] stackArray;
+        private int top;
+        private int capacity;
+
+        public StackArr(int capacity)
+        {
+            stackArray = new T[capacity];
+            this.capacity = capacity;
+            top = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == capacity - 1;
+        }
+
+        public void Push(T data)
+        {
+            if (IsFull())
+            {
+                throw new InvalidOperationException("StackArr is full");
+            }
+
+            stackArray[++top] = data;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("StackArr is empty");
+            }
+
+            T data = stackArray[top--];
+            return data;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("StackArr is empty");
+            }
+
+            return stackArray[top];
+        }
+    }
+
+
+    #endregion
+
+}
