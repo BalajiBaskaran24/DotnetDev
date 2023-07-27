e67f7f545ffa9331b212b6973c4e54cfeb780c33
Balaji-grl <balaji@graniteriverlabs.in>
2023-07-27 22:57:53 +0530

    Added dapper dependency

---
+++ DapperSample/DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace DataAccess.DbAccess;
+
+public class SqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+}
