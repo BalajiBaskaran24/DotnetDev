3b34d72aa4f463505a771064011457c187e16960
Balaji-grl <balaji@graniteriverlabs.in>
2023-07-28 17:00:17 +0530

    Dapper changes
    Note: DB connection string is not working

--- DapperSample/DataAccess/DbAccess/SqlDataAccess.cs
+++ DapperSample/DataAccess/DbAccess/SqlDataAccess.cs
@@ -1,14 +1,47 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DataAccess.DbAccess;
 
-public class SqlDataAccess
+public class SqlDataAccess : ISqlDataAccess
 {
     private readonly IConfiguration _configuration;
 
     public SqlDataAccess(IConfiguration configuration)
     {
         this._configuration = configuration;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(
+        string storedProcedure,
+        U parameters,
+        string connectionId = "Default")
+    {
+        string? connString = _configuration.GetConnectionString(connectionId);
+        IDbConnection connection = new SqlConnection();
+        try
+        {
+            connection = new SqlConnection(connString);
+        }
+        catch (Exception ex)
+        {
+
+        }
+        return await connection.QueryAsync<T>(storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure);
+
+    }
+
+    public async Task SaveData<T>(
+       string storedProcedure,
+       T parameters,
+       string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+
+        await connection.ExecuteAsync(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
     }
 }
