c0070b8156665450922c78f805ece1fd57aa5818
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-02 22:53:42 +0530

    XUnit attribute sample code

---
+++ UnitTestRepo/XUnitReference/AttributesReference.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitReference
+{
+    public class AttributesReference
+    {
+        [Fact]
+        public void ShouldAddTwoNumbers_Fact()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+
+
+        [Theory]
+        [InlineData(3, 3, 6)]
+        [InlineData(2, 2, 4)]
+        public void ShouldAddTwoNumbers_Theory(int a, int b, int expected)
+        {
+            Assert.Equal(expected, a + b);
+        }
+
+
+        [Trait("Category", "Math")]
+        [Fact]
+        public void ShouldAddTwoNumbers_Trait()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+
+
+
+        [Fact(Skip = "Skipping for now")]
+        public void ShouldAddTwoNumbers_Skip()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+
+    }
+}
