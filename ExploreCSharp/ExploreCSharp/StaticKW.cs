a1de901927a3691d4f98f977fd229f8e2edb59ac
Balaji-grl <balaji@graniteriverlabs.in>
2023-08-08 09:11:49 +0530

    Keywords reference

--- ExploreCSharp/ExploreCSharp/StaticKW.cs
+++ ExploreCSharp/ExploreCSharp/StaticKW.cs
@@ -9,15 +9,57 @@ namespace ExploreCSharp
 {
     public class StaticKW
     {
+        //static: Belongs to the type itself rather than to a specific object
+        //Variables local to method cannot be static. Because
+        //the scope of static variable is different whereas scope
+        //of local variables is different. So this cannot be acieved.
+        //But in C its doable
 
-        static int St1 = St2;
-        static int St2;
+        //----------------------------Assign before initialize Static vs reference variables---------------------------------
+        static int St1 = St2;//This will not throw error
+        static int St2 = 10;
+
+        //St1 holds 0
+        //St2 holds 10
+        //St1 = 15;
+        //Now St1 will have 15
 
         //int Ref1 = Ref2;//This line throw error
         //int Ref2;
+        //-----------------------------------------------------------------
 
+        //------------------------------using static for namespaces-----------------------------------
+        //Access static members and nested types
+        //without specifying a type name.
 
-    }
+        //using static System.Console;
+        //using static System.Math;
+        //class Program
+        //{
+        //    static void Main()
+        //    {
+        //        WriteLine(Sqrt(3 * 3 + 4 * 4));//Instead of Math.Sqrt
+        //        }
+        //}
+        //-----------------------------------------------------------------
+
+        //--------------------------------using static for enum---------------------------------
+        //using static Color;
+        //enum Color
+        //{
+        //    Red,
+        //    Green,
+        //    Blue
+        //}
+        //class Program
+        //{
+        //    public static void Main()
+        //    {
+        //        Color color = Green;//Instead of Color.Green
+        //    }
+        //}
+
+}
 
 
 }
