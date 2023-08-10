bae705d8f78a28e2776fb805394fb3297ba4ba80
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-10 23:12:05 +0530

    Exception handling keywords

---
+++ ExploreCSharp/ExploreCSharp/ExceptionHandlingKW.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreCSharp
+{
+    public class ExceptionHandlingKW
+    {
+        public ExceptionHandlingKW() { }
+
+
+        public static void StarterExHandling()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(); //or throw;In this case ex is returned to the called method
+            }
+            finally//Executed always irrespective of catch block
+            {
+
+            }
+        }
+
+    }
+}
