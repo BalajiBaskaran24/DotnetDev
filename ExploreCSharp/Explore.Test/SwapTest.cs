19f6ad74f71ee880893309154b4deb88ac00cc10
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-13 22:00:21 +0530

    MSUnit test changes

--- ExploreCSharp/Explore.Test/SwapTest.cs
+++ ExploreCSharp/Explore.Test/SwapTest.cs
@@ -6,9 +6,14 @@ namespace Explore.Test
         [TestMethod]
         public void Test_Swap_Proper()
         {
+            //Arrange
             int Input1 = 5;
             int Input2 = 6;
+
+            //Act
             ExploreCSharp.Electronics.ExploreEle.Swap(ref Input1, ref Input2);
+            
+            //Assert
             Assert.AreEqual(Input1, 6);
             Assert.AreEqual(Input2, 5);
         }
@@ -16,8 +21,13 @@ namespace Explore.Test
         [TestMethod]
         public void Test_Swap_SameRef()
         {
+            //Arrange
             int Input1 = 5;
+
+            //Act
             ExploreCSharp.Electronics.ExploreEle.Swap(ref Input1, ref Input1);
+            
+            //Assert
             Assert.AreNotEqual(Input1, 0);//The output will be 0. Swap using XOR fails, if we pass same reference as parameter. Since ip1^ip2 returns 0 in step-1
         }
 
