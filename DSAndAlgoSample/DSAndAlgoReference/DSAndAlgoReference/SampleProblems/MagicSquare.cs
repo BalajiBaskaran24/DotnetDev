04fb6b255688922b7776c5a4ccebde0593ab00a7
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-14 22:56:57 +0530

    Solved few hacker rank problems

---
+++ DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/SampleProblems/MagicSquare.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAndAlgoReference.SampleProblems
+{
+    internal class MagicSquare
+    {
+        //Arrange 0 to 9 in an order so that all rows and columns and all diaggonal returns 15 and no repeats
+        //Always 5 will be middle element
+        
+
+    }
+}
