d6dccc0a98f5ca5a5b1b2fdadc027b3391c41605
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-16 08:16:29 +0530

    Added extension method sample

---
+++ ExploreCSharp/ExploreCSharp/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreCSharp
+{
+    /// <summary>
+    /// Class contains extension method
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// First parameter will have "this" keyword. This indicates its a extension method.
+        /// </summary>
+        /// <param name="extClass"></param>
+        /// <param name="input"></param>
+        public static void ExtMethod(this SampleForExt extClass, int input)
+        {
+
+        }
+
+        //This will throw error. Method should be static
+        //public void ExtMethodDup(this SampleForExt extClass, int input){}
+    }
+
+    /// <summary>
+    /// Class for which extension method is written
+    /// </summary>
+    public class SampleForExt
+    {
+
+    }
+
+    /// <summary>
+    /// Class that uses extension
+    /// </summary>
+    public class ExtensionUsage
+    {
+        public void ExtUsage()
+        {
+            SampleForExt ext = new SampleForExt();
+            ext.ExtMethod(0);
+        }
+    }
+
+}
