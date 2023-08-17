8108f89cf3bb51538559657bcc591d8d81e7b6c8
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-17 21:42:51 +0530

    Added few section in algo and Select in LINQ

---
+++ ExploreCSharp/ExploreCSharp/LINQ/ProjectionOperator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreCSharp.LINQ
+{
+    public class ProjectionOperator
+    {
+        public void Usage()
+        {
+            List<Employee> employees = new List<Employee>();
+
+
+            IEnumerable<string> Data = employees.Select(x => x.FirstName);//Select method takes function as parameter. Function 
+                                                                          //takes Employee instance as input and string as output
+
+            //IEnumerable<TempModel> CustomData = employees.Select(x => new { FirstName = x.FirstName, BirthYear = x.YearOfBirth });//Returns an anonymous type that returns
+            //FirstName and YearOfBirth property
+
+            //Above statement will throw error. Cannot convert anonymous type to TempModel
+            //var type can be used for anonymous function
+            var CustomData = employees.Select(x => new { FirstName = x.FirstName, BirthYear = x.YearOfBirth });
+
+
+
+        }
+    }
+
+    public class TempModel
+    {
+        public string FirstName;
+        public int BirthYear;
+    }
+
+
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Designation;
+        public int YearOfBirth;
+        public Employee()
+        {
+
+        }
+    }
+}
