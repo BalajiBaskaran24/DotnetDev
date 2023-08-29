06c61ebda8a4e18d51a89c8b0dfba2421fc053ca
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-08-29 08:57:40 +0530

    Blazor - component
    Assignment completed

---
+++ ExploreBlazorServer/ExploreBlazorServer/Model/ComponentHW/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ExploreBlazorServer.Model.ComponentHW;
+
+public class UserModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Message { get; set; }
+}
