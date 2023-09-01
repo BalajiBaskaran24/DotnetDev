ea736af81d44ca4a17c76bc9ad331f042fccf049
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-09-01 22:29:13 +0530

    Form validation
    commit 1

--- ExploreBlazorServer/ExploreBlazorServer/Model/FormSection/PersonFormModel.cs
+++ ExploreBlazorServer/ExploreBlazorServer/Model/FormSection/PersonFormModel.cs
@@ -1,8 +1,21 @@
-﻿namespace ExploreBlazorServer.Model.FormSection
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExploreBlazorServer.Model.FormSection
 {
     public class PersonFormModel
     {
+        [Required]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "Length Mismatch")]
         public string FirstName { get; set; }
+
+        [Required]
         public string SecondName { get; set; }
+
+        [Range(0, 125, ErrorMessage = "Please enter valid age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "This is must")]
+        public bool GaveConcent { get; set; }
     }
 }
