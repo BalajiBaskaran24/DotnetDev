919ddfb439846da5f8e9871f222740a49c949638
BalajiBaskaran24 <balajibaskaran24@gmail.com>
2023-09-01 21:26:21 +0530

    Forms commit

---
+++ ExploreBlazorServer/ExploreBlazorServer/Model/FormSection/PersonFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ExploreBlazorServer.Model.FormSection
+{
+    public class PersonFormModel
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
