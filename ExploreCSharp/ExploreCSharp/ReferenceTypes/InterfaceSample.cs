b80b65c2d8d7b5350f3d45d3e050d42273147b58
Balaji-grl <balaji@graniteriverlabs.in>
2023-11-27 22:41:47 +0530

    Interface keyword usage

--- ExploreCSharp/ExploreCSharp/ReferenceTypes/InterfaceSample.cs
+++ ExploreCSharp/ExploreCSharp/ReferenceTypes/InterfaceSample.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Diagnostics.Tracing.Parsers;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,12 @@ using System.Threading.Tasks;
 
 namespace ExploreCSharp.ReferenceTypes
 {
+    interface ISample2Interface
+    {
+        int SameMthdName();
+        int SameVarName { get; set; }
+    }
+
     interface ISampleInterface
     {
         void SampleMethod();
@@ -16,13 +23,16 @@ namespace ExploreCSharp.ReferenceTypes
 
         }
 
-        //int Prop { get; private set; } This will throw error
+        //int Prop { get; private set; } This will throw error. Private set is not allowed
 
-        //public bool Sample; Inteface cannot contains instance fields
-        
-        int SampleProp { get; set; }
+        //public bool Sample; Inteface cannot contain instance fields
 
-        
+        int SampleProp { get; set; }//Allowed, since user can define a logic for getter and setter methods
+
+        int SameVarName { get; set; }
+
+        int SameMthdName();
+
         /// <summary>
         /// In new version its possible to define method in interface itself
         /// </summary>
@@ -31,12 +41,48 @@ namespace ExploreCSharp.ReferenceTypes
             Console.WriteLine("Method defined in interface");
         }
     }
-    class ImplementationClass : ISampleInterface
+    class ImplementationClass : ISampleInterface, ISample2Interface
     {
         /// <summary>
         /// Should be implemented as part of ISampleInterface
         /// </summary>
         public int SampleProp { get; set; }
+
+        public int SameName { get; set; }
+
+        public int MyProperty
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+                
+            }
+        }
+
+        public int SameVarName
+        {
+            get
+            {
+                return 0;
+            }
+            set
+            {
+
+            }
+        }
+
+        int ISampleInterface.SameMthdName()
+        {
+            return 0;
+        }
+
+        int ISample2Interface.SameMthdName()
+        {
+            return 0;
+        }
 
         /// <summary>
         /// Should be implemented as part of ISampleInterface
@@ -44,6 +90,14 @@ namespace ExploreCSharp.ReferenceTypes
         void ISampleInterface.SampleMethod()
         {
             // Method implementation
+        }
+    }
+
+    public class InterfaceStarter
+    {
+        public void Starter()
+        {
+
         }
     }
 
