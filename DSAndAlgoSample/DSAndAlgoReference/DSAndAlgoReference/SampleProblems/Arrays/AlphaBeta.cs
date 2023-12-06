c4f025e44a491e6a6d1205f3bb2a18724609a0fb
Balaji-grl <balaji@graniteriverlabs.in>
2023-12-06 12:49:20 +0530

    Update AlphaBeta.cs

--- DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/SampleProblems/Arrays/AlphaBeta.cs
+++ DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/SampleProblems/Arrays/AlphaBeta.cs
@@ -16,19 +16,20 @@ public class AlphaBeta
 {
     public Tuple<int, int> Solution(List<int> input)
     {
+        //Start with Counter value 1. Because counter increases by one for each comparison
         int Counter = 1, Alpha = 0, Beta = 0;
-        for (int i = 0; i < input.Count - 1; i++)
+        for (int i = 0; i < input.Count - 1; i++)//Iterate till n-1 since comparison is involved
         {
-            if (input[i] == input[i + 1])
+            if (input[i] == input[i + 1])//Increment the counter untill consecutive values are same
             {
                 Counter += 1;
             }
             else
             {
-                if (Counter == input[i])
+                if (Counter == input[i])//Update alpha and beta values
                 {
                     Alpha += 1;
-                    if ((i - (Counter - 1)) == input[i])
+                    if ((i - (Counter - 1)) == input[i])//Start index should be equal to value
                     {
                         Beta += 1;
                     }
