95b13cb99f40335f57c7b1e1d5c34d7592c88167
Balaji-grl <balaji@graniteriverlabs.in>
2023-12-15 11:53:32 +0530

    JS DOM Manipulation

--- Webpages/Udemy/JsMyRef/dommanip.js
+++ Webpages/Udemy/JsMyRef/dommanip.js
@@ -3,3 +3,12 @@
 console.log("Dom page loaded");
 
 console.log(document.querySelector(".message"));
+
+console.log("Before manip " + document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "DOM Manipulated text";
+console.log("After manip " + document.querySelector(".message").textContent);
+
+let classNameUserInput = ".userinput";
+console.log("Before manip " + document.querySelector(classNameUserInput).value);
+document.querySelector(classNameUserInput).value = "DOM Manipulated user input";
+console.log("After manip " + document.querySelector(classNameUserInput).value);
