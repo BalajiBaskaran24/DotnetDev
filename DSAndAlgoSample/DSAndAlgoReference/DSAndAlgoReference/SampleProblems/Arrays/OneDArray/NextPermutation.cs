cd1a4f2c866ec168524ff001d5de957f43137a81
Balaji-grl <balaji@graniteriverlabs.in>
2023-12-17 21:54:58 +0530

    Create NextPermutation.cs

---
+++ DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/SampleProblems/Arrays/OneDArray/NextPermutation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAndAlgoReference.SampleProblems.Arrays.OneDArray
+{
+    public class NextPermutation
+    {
+        /// <summary>
+        /// Input: Find the next permutation of: 2,1,3
+        /// Output: 2,3,1
+        /// 
+        /// Input: 3,2,1
+        /// Output: 1,2,3
+        /// 
+        /// All possible permutations
+        /// 1,2,3
+        /// 1,3,2
+        /// 2,1,3
+        /// 2,3,1
+        /// 3,1,2
+        /// 3,2,1
+        /// 
+        /// 
+        /// 
+        /// </summary>
+        public void GetNextPermutation()
+        {
+
+        }
+    }
+}
