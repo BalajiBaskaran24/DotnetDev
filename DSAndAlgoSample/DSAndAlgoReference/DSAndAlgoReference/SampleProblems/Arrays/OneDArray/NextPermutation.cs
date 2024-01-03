a77a0bd25e3ed1b8657aa9aa89fbcdbb664722c7
Balaji-grl <balaji@graniteriverlabs.in>
2024-01-03 11:20:17 +0530

    Update NextPermutation.cs

--- DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/SampleProblems/Arrays/OneDArray/NextPermutation.cs
+++ DSAndAlgoSample/DSAndAlgoReference/DSAndAlgoReference/SampleProblems/Arrays/OneDArray/NextPermutation.cs
@@ -23,12 +23,55 @@ namespace DSAndAlgoReference.SampleProblems.Arrays.OneDArray
         /// 3,1,2
         /// 3,2,1
         /// 
+        ///Input:: 2,1,5,4,3,0,0
+        ///Output:: 2,3,0,0,1,4,5 
+        /// 
+        /// 
         /// 
         /// 
         /// </summary>
-        public void GetNextPermutation()
+        public List<int> GetNextPermutation(List<int> input)
         {
+            int n = input.Count; // size of the array.
 
+            // Step 1: Find the break point:
+            int ind = -1; // break point
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (input[i] < input[i + 1])
+                {
+                    // index i is the break point
+                    ind = i;
+                    break;
+                }
+            }
+
+            // If break point does not exist:
+            if (ind == -1)
+            {
+                // reverse the whole array:
+                input.Reverse(0, input.Count);
+                return input;
+            }
+
+            // Step 2: Find the next greater element
+            //         and swap it with arr[ind]:
+
+            for (int i = n - 1; i > ind; i--)
+            {
+                if (input[i] > input[ind])
+                {
+                    int temp = input[i];
+                    input[i] = input[ind];
+                    input[ind] = temp;
+                    break;
+                }
+            }
+
+            // Step 3: reverse the right half:
+            input.Reverse(ind + 1, input.Count);
+
+            return input;
         }
     }
 }
