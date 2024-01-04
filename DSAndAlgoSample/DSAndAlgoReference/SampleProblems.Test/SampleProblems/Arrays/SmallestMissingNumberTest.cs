d63c4668d3c4751ee5bda801b0a97d1877658657
Balaji-grl <balaji@graniteriverlabs.in>
2024-01-04 21:44:31 +0530

    Buy sell stock sample

--- DSAndAlgoSample/DSAndAlgoReference/SampleProblems.Test/SampleProblems/Arrays/SmallestMissingNumberTest.cs
+++ DSAndAlgoSample/DSAndAlgoReference/SampleProblems.Test/SampleProblems/Arrays/SmallestMissingNumberTest.cs
@@ -1,9 +1,4 @@
-﻿using DSAndAlgoReference.SampleProblems.Arrays;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DSAndAlgoReference.SampleProblems.Arrays.OneDArray;
 
 namespace SampleProblems.Test.SampleProblems.Arrays
 {
