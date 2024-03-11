a3defbd239c96ce088cce8f4d4cb386337d19128
Balaji-grl <balaji@graniteriverlabs.in>
2024-03-11 11:57:40 +0530

    Progress Tracker application

---
+++ React/progresschecker/progresschecker/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/title";
+
+function App() {
+  return (
+    <div className="App">
+      <Title name="React Jonas Course"></Title>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/progresschecker/progresschecker/src/components/task.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "./title";
+
+interface Props {}
+
+const Task: React.FC<Props> = () => {
+  return <div></div>;
+};
+
+export default Task;
