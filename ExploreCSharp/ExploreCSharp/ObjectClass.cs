e6c9de143222af5dd16f0c50a66c9dc8eb8d3e6d
Balaji-grl <balaji@graniteriverlabs.in>
2024-05-13 14:51:41 +0530

    ObjectClass
    Note down all methods related to objects

---
+++ ExploreCSharp/ExploreCSharp/ObjectClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreCSharp
+{
+    internal class ObjectClass
+    {
+
+
+        public override bool Equals(object? obj)
+        {
+            bool RetVal = false;
+            return RetVal;
+        }
+    }
+}
