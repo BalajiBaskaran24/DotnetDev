a0cd80c49c782776761aa9c16fa7f1e2c8b50a8e
Balaji-grl <balaji@graniteriverlabs.in>
2024-05-29 13:01:09 +0530

    Web api changes

--- DSAndAlgoSample/DSAndAlgoReference/SampleProblems.Test/SampleProblems/Arrays/SmallestMissingNumberTest.cs
+++ DSAndAlgoSample/DSAndAlgoReference/SampleProblems.Test/SampleProblems/Arrays/SmallestMissingNumberTest.cs
@@ -4,8 +4,17 @@ namespace SampleProblems.Test.SampleProblems.Arrays
 {
     public class SmallestMissingNumberTest
     {
+
+
+        /// <summary>
+        /// {1,2,3} -> {} {1} {2} {3} {1,2} {2,3} {1,3} {1,2,3} Bitwise OR
+        /// 1,2,3 output:4
+        /// {1,4} -> 1, output:2
+        /// </summary>
+        /// <param name="inputArray"></param>
+        /// <param name="expectedOp"></param>
         [Theory]
-        [InlineData(new int[] { 1, 3 }, 4)]
+        [InlineData(new int[] { 1, 3 }, 2)]
         public void TestCalculateAlphaBeta(int[] inputArray, int expectedOp)
         {
             // Arrange
